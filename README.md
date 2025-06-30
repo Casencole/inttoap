fedeb39e69f66d62b1c853e21670883ea701a6f8
Casencole <casencole2@gmail.com>
2025-06-29 21:51:27 -0400

    inital commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Name
+I Need to Think of a Pun (Inttoap) 
+* Working Title
+
+# Goal
+I am not really sure what my goal is but I would love to use view or edit some
+sort of spread sheet in via the terminal as google sheets is probably my most
+used website. 
+
+To start I would like to run the program on any csv, tsv, etc and have a 
+decently formated output in the terminal (Just viewing no editing)
---
+++ display.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define BUFSIZE 100
+
+int main(int argc, char *argv[]){
+    /**** File Checking ****/
+
+    if (argc < 2) {
+        printf("Ussage: ./display file\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    // TODO: Check for normal file
+    // TODO: Check for supported file type???? or leavel that onto the user?
+
+
+    int fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+        printf("ERROR: Failed to open '%s'\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+    
+    /**** Output ****/
+    char buf[BUFSIZE];
+    int retval;
+    
+    while ( (retval = read(fd, buf, 1)) ) {
+        if (retval == -1) {
+            printf("ERROR: Failed to read '%s'", argv[1]);
+            exit(EXIT_FAILURE);
+        }
+        printf("%c", retval);
+    }
+
+
+    return 0;
+}
